b8db5b5779593a3e8830d598ac0c7ef9839fbfb5
Archana22596 <54067858+Archana22596@users.noreply.github.com>
2019-10-31 14:38:41 +0530

    Example project commiting

---
+++ Example/src/example/demo/E1.java
@@ -0,0 +1,14 @@
+package example.demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class E1 {
+	
+	@Test
+	public void display()
+	{
+		Reporter.log("Life is Beautiful",true);
+	}
+
+}
